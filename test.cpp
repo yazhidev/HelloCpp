ba76912bd9ea338b7acbfba64742a9439e9e1b5d
taki <zengyz@tuya.com>
2022-07-25 13:35:50 +0800

    pointer

--- test.cpp
+++ test.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void test_pointer()
+{
+    int i = 12;
+    int * ip = &i;
+    cout << i << endl; //12
+    cout << ip << endl; //0x16f15b68c
+    cout << *ip << endl; //12，等效于 i
+}
+
 int main(int argc, char* argv[])
 {
    cout << "HelloWorld" << endl;
+   test_pointer();
 }
