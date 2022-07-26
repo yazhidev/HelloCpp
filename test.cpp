45238b9709c4c8cf89bdefac2b82657abe88c215
taki <zengyz@tuya.com>
2022-07-26 08:30:50 +0800

    pointer

--- test.cpp
+++ test.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
-void test_pointer()
-{
+void test_pointer() {
     int i = 12;
-    int * ip = &i;
+    int *ip = &i;
     cout << i << endl; //12
     cout << ip << endl; //0x16f15b68c
     cout << *ip << endl; //12，等效于 i
+
+    int *p, q;
+    q = 10;
+    p = &q;
+    cout << q << endl;
+    cout << p << endl;
+    *p = 100;
+    cout << q << endl;
 }
 
-int main(int argc, char* argv[])
-{
-   cout << "HelloWorld" << endl;
-   test_pointer();
+void func(int *p) {
+    cout << "int func" << endl;
+}
+
+void func(char *p) {
+    cout << "char func" << endl;
+}
+
+int main(int argc, char *argv[]) {
+    cout << "HelloWorld" << endl;
+    test_pointer();
+//   func(nullptr); //nullptr 可以转换为任意指针类型和bool
+    func(0);
 }
